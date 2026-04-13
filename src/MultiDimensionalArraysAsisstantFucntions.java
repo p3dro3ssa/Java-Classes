f643e6473041a0fc198d95050ebbd35b20772790
Paulina1765 <paulinafurtek@spoko.pl>
2026-04-13 14:44:23 +0200

    MultiDimensionalArraysAssistantFunctions Class created with few methods

---
+++ src/MultiDimensionalArraysAsisstantFucntions.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import static java.lang.IO.*;
+
+public class MultiDimensionalArraysAsisstantFucntions {
+    Scanner input = new Scanner(System.in);
+    public void fill2DArray(int[][] array, int n, int m){
+        for(int i=0; i<n;i++){
+            for(int j=0;j<m;j++){
+                array[i][j]= input.nextInt(11);
+            }
+        }
+    }
+    public void sumOfSquareArrayDiagonal(int[][] array, int n) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += array[i][i];
+        }
+    }
+}
