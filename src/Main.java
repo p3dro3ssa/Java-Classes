09288352fbe2cc380a4b520dbf0d6021112321ca
p3dro3ssa <pedro.krzyszczak@gmail.com>
2026-04-20 13:47:18 +0200

    MultiDimensionalArraysAssistant Functions class added and Main.java changed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import static java.lang.IO.*;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 //void main() {
 //   Product product = new Product();
 //    product.setName("laptop");
@@ -8,29 +7,38 @@ import static java.lang.IO.*;
 //    product.setPriceNetto(4500);
 //   product.setQuantity(50);
 //  product.Display();
-//
 //}
 
-void main(String[] args){
-    Scanner sc=new Scanner(System.in);
-    ArraysAssistantFunctions af = new ArraysAssistantFunctions();
-    ArraysAssistantFunctions.ExtremumIndex ex1= af.new ExtremumIndex();
-    int size;
-    do{
-        size = sc.nextInt();
-    }while(size<=0);
-    int[] tab = new int[size];
-    for(int i=0;i<size;i++){
-        tab[i]=sc.nextInt();
+//void main(String[] args){
+//    Scanner sc=new Scanner(System.in);
+//    ArraysAssistantFunctions af = new ArraysAssistantFunctions();
+//    ArraysAssistantFunctions.ExtremumIndex ex1= af.new ExtremumIndex();
+//    int size;
+//    do{
+//        size = sc.nextInt();
+//    }while(size<=0);
+//    int[] tab = new int[size];
+//    for(int i=0;i<size;i++){
+//        tab[i]=sc.nextInt();
+//    }
+//    ex1 = af.findMin(tab);
+//    int avg = af.calculateAvg(tab);
+//    for(int i=0;i<size;i++){
+//        if(tab[i]<avg){
+//            println(tab[i]);
+//        }
+//    }
+//    int[] tab2 = new int[size];
+//    tab2 = Arrays.copyOf(tab,size);
+//
+//}
+void main(){
+    int n;
+    Scanner input = new Scanner(System.in);
+    n = input.nextInt();
+    int[][] array = new int[n][n];
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum+=array[i][i];
     }
-    ex1 = af.findMin(tab);
-    int avg = af.calculateAvg(tab);
-    for(int i=0;i<size;i++){
-        if(tab[i]<avg){
-            println(tab[i]);
-        }
-    }
-    int[] tab2 = new int[size];
-    tab2 = Arrays.copyOf(tab,size);
-    
 }
